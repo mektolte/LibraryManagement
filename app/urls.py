58d678a4305a0b443a4787075aa3196aa595c59a
mektolte <rakaig.123@gmail.com>
2021-05-05 19:59:15 +0700

    Book list done

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('books', views.all_books, name='books'),
+    path('add_book', views.add_book, name='add-book'),
+    path('update_book/<book_id>', views.update_book, name='update-book'),
+    path('delete_book/<book_id>', views.delete_book, name='delete-book'),
+    path('Lenders', views.all_lender, name='lender'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import BookForm
+
 
 # Create your views here.
 
@@ -12,4 +15,50 @@ def all_books(request):
     book_list = Book.objects.all()
     return render(request, 'app/book_list.html', {
         'book_list': book_list,
+    })
+
+
+def add_book(request):
+    form = BookForm
+    submitted = False
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('books')
+        else:
+            form = BookForm(request.POST)
+            if 'submitted' in request.GET:
+                submitted = True
+
+    return render(request, 'app/add_book.html', {
+        'form': form,
+        'submitted': submitted,
+    })
+
+
+def update_book(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('books')
+
+    return render(request, 'app/update_book.html', {
+        'book': book,
+        'form': form,
+    })
+
+
+def delete_book(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    book.delete()
+    return HttpResponse('Deleted')
+
+
+def all_lender(request):
+    lender_list = Lender.objects.all()
+    return render(request, 'app/lender_list.html', {
+        'lender_list': lender_list,
     })
